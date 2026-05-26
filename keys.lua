a9c838f3f98a12beb528c6676dcbcffbd794142d
Vermex221 <harryewilcox@gmail.com>
2026-05-26 19:23:48 +0100

    Add files via upload

---
+++ keys.lua
@@ -0,0 +1,15 @@
+-- PFSploit Valid Keys
+-- Add or remove keys from this list as needed
+
+return {
+    "PFS-a8Xk2-Qm9Tz-Rv4Lp",
+    "PFS-jN7Wy-Hc3Bf-Xs6Dg",
+    "PFS-eR5Vn-Uw8Jt-Kp2Mq",
+    "PFS-bG4Lx-Yz9Cr-Fn7Wh",
+    "PFS-mT6Ds-Jv3Pk-Qw8Xn",
+    "PFS-cH9Rf-Lg2Ky-Bx5Zt",
+    "PFS-wN3Jp-Tv7Mq-Ds4Lf",
+    "PFS-kX8Bg-Ry6Wn-Hm2Vc",
+    "PFS-fQ5Tz-Cx9Lh-Jw7Pk",
+    "PFS-sV2Mn-Gp4Xr-Ky8Bt",
+}
